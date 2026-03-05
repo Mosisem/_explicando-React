c9471b8c7926c0c8d120eab3a6d4d4c2a1afa42f
Mosi sebastião <mosijoao4002@gmail.com>
2026-03-05 19:09:12 +0000

    jhbjhjh

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/explicando-React/',
+  base: 'https://mosisem.github.io/_explicando-React/',
 })
